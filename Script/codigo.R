998cc2fabad69723e855ecb32b4964b2f393838b
DiegoSalazarA <63475204+DiegoSalazarA@users.noreply.github.com>
2020-06-05 19:53:26 -0400

    Add files via upload

---
+++ Script/codigo.R
@@ -0,0 +1,109 @@
+#Codigo de trabajo "Ciencia Abierta y Software Libre"
+#Estudiante: Diego Salazar Alvarado
+
+
+
+# ---- 0. Aspectos Generales ----
+
+
+
+#Cargar paquetes
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(car)
+
+#Creamos objeto desde la base de datos, seleccionado hoja n?mero 2
+actoresproy1820 <- read_excel("Data/proy_usach.xlsx",2)
+
+#Revisar base
+names(actoresproy1820)
+summary(actoresproy1820)
+
+# ---- 1. Preparar objetos para el analisis ----
+
+#Seleccion de varibles
+actores <- select(actoresproy1820, Inicio, ID, titulo,
+                  Facultad, Sector, Tipo, Comuna, Region)
+
+#unir ID y fecha de inicio, para generar un nuevo ID que incluya el a?o
+
+actores <- unite(actores, Inicio, ID, c(1:2), sep= "") 
+actores <- select(actores, ID = Inicio, titulo, Facultad, Sector, Tipo, Comuna, Region)
+
+#Crea objeto para  RM, eliminando los casos internacionales y de otras regiones
+actoresRM <- filter(actores, Region == "RM")
+
+#Crear Columna Zona, desde la categorizaci?n de las comunas en tres circulos concentricos
+# EI: Entorno inmediato; CC2: otras comunas dentro de cicunvalacion vespucio
+# CC3: Comunas fuera de cicunvalacion vespucio
+
+actoresRM <- mutate(actoresRM, zona = car::recode(actoresRM$Comuna,
+                                                  "'Estaci?n Central' = 'EI'; 'Cerrillos' = 'EI'; 'Maip?'= 'EI'; 
+            'Pudahuel' = 'EI'; 'Lo Prado' = 'EI'; 'Quinta Normal' = 'EI';
+            'Santiago'= 'EI'; 'Pedro Aguirre Cerda' = 'EI'; '?u?oa' = 'CC2'; 
+            'Cerro Navia' = 'CC2';'La Granja' = 'CC2'; 'Providencia' = 'CC2'; 
+            'Conchal?' = 'CC2'; 'La Cisterna' = 'CC2'; 'San Miguel' = 'CC2';
+            'San Bernardo' = 'CC3'; 'Isla de Maipo' = 'CC3';'Huechuraba' = 'CC3';
+            'La Florida' = 'CC3'; 'Las Condes' = 'CC3'; 'Vitacura' = 'CC3'; 'La Pintana' = 'CC3';
+            'Puente Alto' = 'CC3' ; 'San Jos? de Maipo' = 'CC3'; 'El Bosque' = 'CC3';
+            'Lampa' = 'CC3';'Melipilla' = 'CC3'; 'Quilicura' = 'CC3'"))
+
+#Guardar base de datos
+saveRDS(actoresRM, file = "Output/actoresrm.rds" ,compress = F )
+
+# ---- 2. Analisis ----
+
+# ---- 2.1 Analisis por distribuci?n territorial  ----
+
+#  Por Comuna (Estainformaci?n queda disponible para el desarrollo del mapa)
+
+table(actoresRM$Comuna)
+
+#Comuna y Sector
+
+table(actoresRM$Comuna, actoresRM$Sector)
+round(prop.table(table(actoresRM$Comuna, actoresRM$Sector))*100, digits = 1)
+
+#  Por Zona y Sector
+
+table(actoresRM$zona,actoresRM$Sector)
+round(prop.table(table(actoresRM$zona,actoresRM$Sector))*100, digits = 1)
+
+# Por Zona y Facultad
+
+table(actoresRM$Facultad,actoresRM$zona)
+round(prop.table(table(actoresRM$Facultad,actoresRM$zona))*100, digits = 1)
+
+# ---- 2.2 Analisis por Sector ----
+
+
+#Analisis por Sector y Tipo de actor
+
+table(actoresRM$Sector)
+round(prop.table(table(actoresRM$Sector))*100, digits = 1)
+
+# por Sector y Facultad
+
+table(actoresRM$Facultad, actoresRM$Sector)
+round(prop.table(table(actoresRM$Facultad, actoresRM$Sector))*100, digits = 1)
+
+
+# Creamos objeto por cada uno de los Sectores, para analisas sus subsectores
+
+EducacionRM <- filter(actoresRM, Sector == "Educacion")
+table(EducacionRM$Tipo)
+round(prop.table(table(EducacionRM$Tipo, EducacionRM$zona))*100, digits = 1)
+
+ProductivosRM <- filter(actoresRM, Sector == "Productivo")
+table(ProductivosRM$Tipo)
+round(prop.table(table(ProductivosRM$Tipo, ProductivosRM$zona))*100, digits = 1)
+
+PublicosRM <- filter(actoresRM, Sector == "publico")
+table(PublicosRM$Tipo)
+round(prop.table(table(PublicosRM$Tipo, PublicosRM$zona))*100, digits = 1)
+
+SocialesRM <- filter(actoresRM, Sector == "Social")
+table(SocialesRM$Tipo)
+round(prop.table(table(SocialesRM$Tipo, SocialesRM$zona))*100, digits = 1)
+
