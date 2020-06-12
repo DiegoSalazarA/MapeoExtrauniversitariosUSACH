ceaefad2f56b0635cef8c029fa80757b50f13130
DiegoSalazarA <63475204+DiegoSalazarA@users.noreply.github.com>
2020-06-12 01:58:38 -0400

    Add files via upload

---
+++ Script/Analysis/AnalysisScript.R
@@ -0,0 +1,88 @@
+# ---- 0. Aspectos Generales ----
+
+
+
+#Cargar paquetes
+library(dplyr)
+library(ggplot2)
+library(summarytools)
+
+#Abrimos base para el analisis  creamos objeto
+actoresRM <- readRDS("Data/Analysis Data/actoresrm.rds")
+
+# ---- 1. Analisis ----
+
+# ---- 1.1 Analisis por distribucion territorial  ----
+
+#  Por Comuna (Esta informacion queda disponible para el desarrollo del mapa)
+
+freq(actoresRM$Comuna)
+
+# Tablas cruzando Comuna y Sector (publico,social,productivo o educativo no universitario)
+# prop = "c" se usa para que la proporcion se presente en las columnas.
+
+ctable(actoresRM$Comuna, actoresRM$Sector, prop = "t" , totals = F)
+
+#  Por Zona y Sector
+
+ctable(actoresRM$zona,actoresRM$Sector,  prop = "t" , totals = F)
+
+# Por Zona y Facultad
+
+ctable(actoresRM$Facultad,actoresRM$zona, prop = "t" , totals = F)
+
+# ---- 2.2 Analisis por Sector ----
+
+#Analisis por Sector y subsector de actor
+
+freq(actoresRM$Sector)
+
+# por Sector y Facultad
+
+ctable(actoresRM$Facultad, actoresRM$Sector, prop = "c" , totals = F)
+
+round(prop.table(table(actoresRM$Facultad, actoresRM$Sector))*100, digits = 1)
+
+
+# Creamos objetos por cada uno de los Sectores, para analisar sus subsectores
+
+EducacionRM <- filter(actoresRM, Sector == "Educacion")
+freq(EducacionRM$Tipo)
+ctable(EducacionRM$Tipo, EducacionRM$zona, prop = "t" , totals = T)
+
+
+ProductivosRM <- filter(actoresRM, Sector == "Productivo")
+freq(ProductivosRM$Tipo)
+ctable(ProductivosRM$Tipo, ProductivosRM$zona, prop = "t" , totals = T)
+
+PublicosRM <- filter(actoresRM, Sector == "publico")
+freq(PublicosRM$Tipo)
+ctable(PublicosRM$Tipo, PublicosRM$zona, prop = "t" , totals = T)
+
+SocialesRM <- filter(actoresRM, Sector == "Social")
+freq(SocialesRM$Tipo)
+ctable(SocialesRM$Tipo, SocialesRM$zona, prop = "t" , totals = T)
+
+# ---- 3. Imprimir Resultados ----
+
+# Gráfico de Cantidad de actores por zona
+
+ggplot(actoresRM, aes(x=zona_factor)) +
+  geom_bar(width = 0.3,  fill=rgb(1,0,0.4,0.8)) +
+  scale_x_discrete("Circulo Concentrico") + scale_y_continuous("Cantidad de Actores") + 
+  labs(title = "Cantidad de actores por zona")
+
+ggsave("Output/actoresporzona.png")
+
+# Gráfico de Cantidad de actores por Sector
+
+actoresRM <- mutate(actoresRM, sector_factor = factor(actoresRM$Sector,
+                                                      labels = c("Educacion","Productivo",
+                                                                 "Publico","Social"))) 
+
+ggplot(actoresRM, aes(x=sector_factor)) + 
+  geom_bar(width = 0.3,  fill=rgb(1,0,0.4,0.8)) +
+  scale_x_discrete("Sector") + scale_y_continuous("Cantidad de Actores") + 
+  labs(title = "Cantidad de actores por sector")
+
+ggsave("Output/actoresporsector.png")
---
+++ Script/Proccesing/ProccesingScript.R
@@ -0,0 +1,59 @@
+#Codigo de trabajo "Ciencia Abierta y Software Libre"
+#Estudiante: Diego Salazar Alvarado
+
+
+
+# ---- 0. Aspectos Generales ----
+
+
+
+#Cargar paquetes
+library(readxl)
+library(dplyr)
+library(car)
+
+
+#Creamos objeto desde la base de datos, seleccionado hoja numero 2
+actoresproy1820 <- read_excel("Data/Original Data/proy_usach.xlsx",2)
+
+#Revisar base
+names(actoresproy1820)
+summary(actoresproy1820)
+
+# ---- 1. Preparar objetos para el analisis ----
+
+#Seleccion de varibles
+actores <- select(actoresproy1820, Inicio, ID, titulo,
+                  Facultad, Sector, Tipo, Comuna, Region)
+
+#unir ID y fecha de inicio, para generar un nuevo ID que incluya el año en el mismo vector
+
+actores <- unite(actores, Inicio, ID, c(1:2), sep= "") 
+actores <- select(actores, ID = Inicio, titulo, Facultad, Sector, Tipo, Comuna, Region)
+
+#Crea objeto para  RM, eliminando los casos internacionales y de otras regiones
+actoresRM <- filter(actores, Region == "RM")
+
+#Crear Columna Zona, desde la categorizacion de las comunas en tres circulos concentricos
+# EI: Entorno inmediato
+# CC2: otras comunas dentro de cicunvalacion vespucio
+# CC3: Comunas fuera de cicunvalacion vespucio
+
+actoresRM <- mutate(actoresRM, zona = car::recode(actoresRM$Comuna,
+                                                  "'Estacion Central' = 'EI'; 'Cerrillos' = 'EI'; 'Maipu'= 'EI'; 
+            'Pudahuel' = 'EI'; 'Lo Prado' = 'EI'; 'Quinta Normal' = 'EI';
+            'Santiago'= 'EI'; 'Pedro Aguirre Cerda' = 'EI'; 'Nunoa' = 'CC2'; 
+            'Cerro Navia' = 'CC2';'La Granja' = 'CC2'; 'Providencia' = 'CC2'; 
+            'Conchali' = 'CC2'; 'La Cisterna' = 'CC2'; 'San Miguel' = 'CC2';
+            'San Bernardo' = 'CC3'; 'Isla de Maipo' = 'CC3';'Huechuraba' = 'CC3';
+            'La Florida' = 'CC3'; 'Las Condes' = 'CC3'; 'Vitacura' = 'CC3'; 'La Pintana' = 'CC3';
+            'Puente Alto' = 'CC3' ; 'San Jose de Maipo' = 'CC3'; 'El Bosque' = 'CC3';
+            'Lampa' = 'CC3';'Melipilla' = 'CC3'; 'Quilicura' = 'CC3'"))
+
+# Transformamos el vestor zona en factor, las etiquetas se ordenan segun el alfabeto
+actoresRM <- mutate(actoresRM, zona_factor = factor(actoresRM$zona,
+                                                    labels = c("CC2","CC3","EI"))) 
+
+
+#Guardar base de datos con los casos RM
+saveRDS(actoresRM, file = "Data/Analysis Data/actoresrm.rds" ,compress = F )
