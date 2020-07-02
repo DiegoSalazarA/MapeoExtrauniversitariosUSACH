16ddefad257f257f5ff41529f8453299c00f1007
DiegoSalazarA <63475204+DiegoSalazarA@users.noreply.github.com>
2020-07-02 18:39:08 -0400

    Add files via upload

--- Script/Proccesing/ProccesingScript.R
+++ Script/Proccesing/ProccesingScript.R
@@ -12,25 +12,29 @@ library(readxl)
 library(tidyverse)
 library(dplyr)
 library(car)
-library(carData)
+
 
 #Creamos objeto desde la base de datos, seleccionado hoja numero 2
-actoresproy1820 <- read_excel("Data/Original Data/Proy_usach.xlsx", sheet = 2)
+basefvime <- read_excel("Data/Original Data/Proy_usach.xlsx", sheet = 2)
 
 #Revisar base
-names(actoresproy1820)
-summary(actoresproy1820)
+names(basefvime)
+summary(basefvime)
+dim(basefvime)
+str(basefvime)
 
 # ---- 1. Preparar objetos para el analisis ----
 
 #Seleccion de varibles
-actores <- select(actoresproy1820, Inicio, ID, titulo,
+basefvime <- select(basefvime, Inicio, ID, titulo, area,
                   Facultad, Sector, Tipo, Comuna, Region)
 
 #unir ID y fecha de inicio, para generar un nuevo ID que incluya el año en el mismo vector
 
-actores <- unite(actores, Inicio, ID, c(1:2), sep= "") 
-actores <- select(actores, ID = Inicio, titulo, Facultad, Sector, Tipo, Comuna, Region)
+basefvime <- unite(basefvime, Inicio, ID, c(1:2), sep= "") 
+
+#Cambiamos el nombre a la nueva ID por "ID"
+actores <- select(basefvime, ID = Inicio, titulo, area, Facultad, Sector, Tipo, Comuna, Region)
 
 #Crea objeto para  RM, eliminando los casos internacionales y de otras regiones
 actoresRM <- filter(actores, Region == "RM")
@@ -41,17 +45,25 @@ actoresRM <- filter(actores, Region == "RM")
 # CC3: Comunas fuera de cicunvalacion vespucio
 
 actoresRM <- mutate(actoresRM, zona = car::recode(actoresRM$Comuna,
-            "'Estacion Central' = 'EI'; 'Cerrillos' = 'EI'; 'Maipu'= 'EI'; 
-            'Pudahuel' = 'EI'; 'Lo Prado' = 'EI'; 'Quinta Normal' = 'EI';
+            "'Estacion Central' = 'EI'; 'Cerrillos' = 'EI'; 
+            'Lo Prado' = 'EI'; 'Quinta Normal' = 'EI';
             'Santiago'= 'EI'; 'Pedro Aguirre Cerda' = 'EI'; 
             
-            'Nunoa' = 'CC2'; 'Cerro Navia' = 'CC2';'La Granja' = 'CC2'; 'Providencia' = 'CC2'; 
-            'Conchali' = 'CC2'; 'La Cisterna' = 'CC2'; 'San Miguel' = 'CC2';
             
-            'San Bernardo' = 'CC3'; 'Isla de Maipo' = 'CC3';'Huechuraba' = 'CC3';
+            'Cerro Navia' = 'CC2';'La Granja' = 'CC2'; 'Providencia' = 'CC2'; 
+            'Conchali' = 'CC2'; 'La Cisterna' = 'CC2'; 'San Miguel' = 'CC2';'Maipu'= 'CC2';
+            'Independencia' = 'CC2'; 'Lo Espejo' = 'CC2'; 'Macul' = 'CC2'; 'Maipu' ='CC2';
+            'Nunoa' = 'CC2'; 'Pudahuel' = 'CC2'; 'Recoleta' = 'CC2'; 'Renca' = 'CC2';
+            'San Joaquin' = 'CC2'; 'San Ramon' = 'CC2';
+            
+            
+            'San Bernardo' = 'CC3'; 'Huechuraba' = 'CC3';
             'La Florida' = 'CC3'; 'Las Condes' = 'CC3'; 'Vitacura' = 'CC3'; 'La Pintana' = 'CC3';
-            'Puente Alto' = 'CC3' ; 'San Jose de Maipo' = 'CC3'; 'El Bosque' = 'CC3';
-            'Lampa' = 'CC3';'Melipilla' = 'CC3'; 'Quilicura' = 'CC3'"))
+            'Puente Alto' = 'CC3' ; 'El Bosque' = 'CC3';
+            'Quilicura' = 'CC3'; 'La Reina' = 'CC3';
+            'Penalolen' = 'CC3'; 
+            
+            'Isla de Maipo' = 'CC4';'Lampa' = 'CC4';'Melipilla' = 'CC4';'San Jose de Maipo' = 'CC4'"))
 
 #Crea Columa de prioridad según indice de prioridad social
 
@@ -71,15 +83,31 @@ actoresRM <- mutate(actoresRM, IPS = car::recode(actoresRM$Comuna,
                                                 'San Miguel' = 'Baja';
                                                 'Nunoa' = 'Sin Prioridad';'Providencia'  = 'Sin Prioridad' ; 'Las Condes'  = 'Sin Prioridad';
                                                 'Vitacura' = 'Sin Prioridad'"))
-                                                 
+
 
 # Transformamos los vestores zona e IPS en factor, las etiquetas se ordenan segun el alfabeto
-actoresRM <- mutate(actoresRM, zona_factor = factor(actoresRM$zona,
-                                                    labels = c("CC2","CC3","EI"))) 
 
-actoresRM <- mutate(actoresRM, IPS_factor = factor(actoresRM$IPS,
+actoresRM <- mutate (actoresRM, Area = as.factor(actoresRM$area)) 
+
+actoresRM <- mutate (actoresRM, Comuna = as.factor(actoresRM$Comuna)) 
+
+actoresRM <- mutate (actoresRM, Sector = as.factor(actoresRM$Sector)) 
+
+actoresRM <- mutate (actoresRM, Subsector = as.factor(actoresRM$Tipo))
+
+actoresRM <- mutate(actoresRM, Circunvalación = factor(actoresRM$zona,
+                                                    labels = c("Circunvalación Proxima","Circunvalación Media","Fuerda de la Ciudad","Entorno inmediato"))) 
+
+actoresRM <- mutate(actoresRM, IPS = factor(actoresRM$IPS,
                                                     labels = c("Alta","Baja","Media Alta", "Media Baja", "Sin Prioridad"))) 
+
+
+# Seleccionamos las variables Definitivas
+
+actoresRM <-select(actoresRM, ID, titulo, Area,
+                   Facultad, Sector, Subsector, Comuna, Circunvalación, IPS,zona)
 
 
 #Guardar base de datos con los casos RM
 saveRDS(actoresRM, file = "Data/Analysis Data/actoresrm.rds" ,compress = F )
+
